411211a83f3d0ef00e2879c8ff13495fd486c30b
nicolaas @ tappy <github@sunnysideup.co.nz>
2022-01-25 06:39:14 +1300

    WIP

--- src/Extensions/MemberExtension.php
+++ src/Extensions/MemberExtension.php
@@ -2,6 +2,8 @@
 
 namespace Sunnysideup\EcommerceAdvanceRetailConnector\Extensions;
 
+use SilverStripe\Forms\ReadonlyField;
+use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
 
 class MemberExtension extends DataExtension
@@ -9,4 +11,17 @@ class MemberExtension extends DataExtension
     private static $db = [
         'AdvanceRetailCustomerID' => 'Int',
     ];
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $owner = $this->getOwner();
+        $fields->addFieldsToTab(
+            'Root.AdvanceRetail',
+            ReadonlyField::create(
+                'AdvanceRetailCustomerID',
+                'AR Customer ID'
+            )
+        );
+        return $fields;
+    }
 }
